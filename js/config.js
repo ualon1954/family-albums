38adc2d2370a8c3c39625ecf825ee9ce79cb15fe
ualon <ualon1954@gmail.com>
2026-09-02 11:10:31 +0300

    update permissions

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // V3 — paste your deployed Google Apps Script Web App URL here.
 window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxPAaEQA4W_9xmbVQJhZfWOmNN1VKL2bmdomfpazyRFbWfWdK14_C5yqdWNwJkm1Lj9/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbwExwXuEFFjPybxdb0UcPon27vVMTVxVuVq8SM6CKvpyFb81ziNg7U_XhXDO9Vyqv6Q/exec"
 };
